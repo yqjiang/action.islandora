868a93760d6efd9e20bf2e5e50792ff518c1d975
mitchmac <mitchmac@gmail.com>
2013-07-17 16:28:00 -0300

    Initial commit

---
+++ class.Islandora.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('AJXP_EXEC') or die('Access not allowed');
+
+class Islandora extends AJXP_Plugin{
+
+  public function switchAction($action, $httpVars, $fileVars) {
+
+  }
+}
---
+++ i18n/en.php
@@ -0,0 +1,8 @@
+<?php
+
+$mess = array(
+  "1" => "Islandora",
+  "2" => "Title",
+);
+
+?>
