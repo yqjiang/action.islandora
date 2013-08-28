091e3181dfea9ef00235ea1a98c340b3ae915f3d
phil <phil@discoverygarden.ca>
2013-08-28 15:47:04 -0300

    t4

--- class.Islandora.php
+++ class.Islandora.php
@@ -5,6 +5,10 @@ defined('AJXP_EXEC') or die('Access not allowed');
 class Islandora extends AJXP_Plugin{
 
   public function switchAction($action, $httpVars, $fileVars) {
-    AJXP_XMLWriter::sendMessage("Successfully islandora ingest", null);
+    AJXP_XMLWriter::header();
+    AJXP_XMLWriter::sendMessage("Successfully ingest", null);
+    AJXP_XMLWriter::reloadDataNode();
+    AJXP_XMLWriter::close();
+
   }
 }
