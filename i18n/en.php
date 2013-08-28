2cda554c9aea18e07ce90f83abbf312551b49de8
phil <phil@discoverygarden.ca>
2013-08-28 14:13:05 -0300

    form test

--- i18n/en.php
+++ i18n/en.php
@@ -1,8 +1,12 @@
 <?php
 
 $mess = array(
-  "1" => "Islandora",
-  "2" => "Title",
+  "title" => "Islandora",
+  "1" => "field 1",
+  "1d" => "field 1 description",
+  "2" => "field 2",
+  "2d" => "field 2 description",
+  
 );
 
 ?>
