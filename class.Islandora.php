61181c0ed5b18669af37acff416f151e49e5621a
phil <phil@discoverygarden.ca>
2013-08-28 14:30:58 -0300

    callback

--- class.Islandora.php
+++ class.Islandora.php
@@ -5,6 +5,6 @@ defined('AJXP_EXEC') or die('Access not allowed');
 class Islandora extends AJXP_Plugin{
 
   public function switchAction($action, $httpVars, $fileVars) {
-
+    echo("hello");
   }
 }
