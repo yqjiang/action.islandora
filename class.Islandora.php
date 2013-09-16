ea0aa5ad30f09c02b12a7ecba90328504c1a868b
phil <phil@discoverygarden.ca>
2013-09-16 13:10:33 -0300

    adding fields for metadata form

--- class.Islandora.php
+++ class.Islandora.php
@@ -9,9 +9,7 @@ class Islandora extends AJXP_Plugin{
     $f1 = $httpVars["field1"];
     $f2 = $httpVars["field2"];
     AJXP_XMLWriter::header();
-    AJXP_XMLWriter::sendMessage("Successfully ingest", null);
-    AJXP_XMLWriter::sendMessage("field 1 value: $f1", null);
-    AJXP_XMLWriter::sendMessage("field 2 value: $f2", null);
+    AJXP_XMLWriter::sendMessage("Successfully ingest \n field 1 value: $f1 \n field 2 value: $f2", null);
    
     //AJXP_XMLWriter::reloadDataNode();
     AJXP_XMLWriter::close();
