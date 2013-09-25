3bea8e23a6dac1dd4de67eb8688fe9260c962d58
Adam Vessey <adam@discoverygarden.ca>
2013-09-25 19:10:59 +0000

    Get jQuery imported; aborted approach.

--- class.Islandora.js
+++ class.Islandora.js
@@ -1,4 +1,31 @@
 Class.create("Islandora", {
+  initialize: function () {
+    this.mgr = new ResourcesManager();
+  },
+  loadDatepicker: function() {
+    if (typeof jQuery === 'undefined') {
+      this.mgr.loadJSResource('plugins/action.sfudora/res/js/jquery.js', 'jquery_core');
+    }
+    if (!jQuery.hasOwnProperty("datepicker")) {
+      this.mgr.loadJSResource('plugins/action.sfudora/res/lib/jquery-ui/ui/jquery.ui.core.js', 'jquery_ui_core');
+      this.mgr.loadJSResource('plugins/action.sfudora/res/lib/jquery-ui/ui/jquery.ui.datepicker.js', 'jquery_ui_datepicker');
+      this.mgr.loadCSSResource('plugins/action.sfudora/res/lib/jquery-ui/themes/base/jquery.ui.all.css');
 
+      // Apply some default options, if none are provided.
+      defaults = {
+        dateFormat: jQuery.datepicker.W3C,
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "c-10:c+3"
+      };
+      jQuery.datepicker.setDefaults(defaults);
+    }
+  },
+  addDatepicker: function (selector, options) {
+    this.loadDatepicker();
 
+    options = typeof options !== 'undefined' ? options : {};
+
+    jQuery(selector).datepicker(options);
+  }
 });
